f9fb26dd8fe5d60db90645cc62c21012f4bbfd3f
DavidCalcara <38891911+DavidCalcara@users.noreply.github.com>
2018-09-17 13:58:25 -0400

    Add files via upload

---
+++ decimaltobinary.py
@@ -0,0 +1,18 @@
+"""
+File: decimaltobinary.py
+Author: David
+Converts a decimal integer to a string of bits.
+"""
+
+decimal = int(input("Enter a decimal integer: "))
+if decimal == 0:
+	print(0)
+else:
+	print("Quotient Remainder Binary")
+	bitString = ""
+	while decimal > 0:
+		remainder = decimal % 2
+		decimal = decimal // 2
+		bitString = str(remainder) + bitString
+		print("%5d%8d%12s" % (decimal, remainder, bitString))
+	print("The Binary representation is", bitString)
